13b43803544294017f103a2b8341700285cb2593
Alshoja <alshoja@gmail.com>
2020-05-20 19:24:22 +0530

    Observer for responsive design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { BreakpointObserverService } from './../breakpoint-observer.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'breakpointObserver';
+export class AppComponent implements OnInit {
+  title = 'Break-Point-Observer';
+  public screenSize: any;
+
+  constructor(private ObserverService: BreakpointObserverService) { }
+
+  ngOnInit() {
+    this.detectScreenChange();
+  }
+
+  detectScreenChange() {
+    this.ObserverService.size$.subscribe(breakPoint => {
+      this.screenSize = breakPoint;
+    });
+  }
 }
